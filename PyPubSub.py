bde53961343ecefd3c668d8d5ae4d35baf8399f2
joshbyrom <joshbyrom@gmail.com>
2012-12-20 19:48:15 -0500

    added once command and a wrapper for callback functions

--- PyPubSub.py
+++ PyPubSub.py
@@ -1,17 +1,67 @@
 class PubSub(object):
+    """ A publisher/subscriber object used as a means of keeping coupling loose. """
     def __init__(self):
         self.handlers = {}
 
-    def on(self, event, fun):
+    def on(self, event, fun, infinite=True, repeats=0):
+        """ Adds a subscriber function to this object. The function will be called
+            at least once when the event occures, regardless of the repears parameter.
+
+            Argument Details:
+            event -- the event name, a string
+            fun -- the callback function to call when the event is emitted
+            infinite -- if true, callback function will always be called
+            repeats -- the number of times to repeat the function call
+
+        """
         if not event in self.handlers:
             self.handlers[event] = []
+            
+        wrapper = FunctionWrapper(fun, infinite, repeats)
+        
+        self.handlers[event].append(wrapper)
 
-        self.handlers[event].append(fun)
+    def once(self, event, fun):
+        """ Use when an event listener or subscriber only needs to be fired once.
+
+            Argument Details:
+            event -- the event name, a string
+            fun -- the callback function to call when the event is emitted
+
+        """
+        self.on(event, fun, False)
 
     def emit(self, event, args=[]):
+        """ Use to signal that an event has occured.
+
+
+            Argument Details:
+            event -- the event name, a string
+            args -- variable arguments to pass to the callbacks of this event
+        """
         if event in self.handlers:
-            for fun in self.handlers[event]:
-                fun(self, event, args)
+            for wrapper in self.handlers[event]:
+                wrapper.call(self, event, *args)
+            self.handlers[event] = [x for x in self.handlers[event] if not x.finished]
+
+    class FunctionWrapper(object):
+        """ An inner class used by the PubSub class to
+            wrap subscriber functions, and limit their use."""
+        def __init__(self, fun, infinite, repeats):
+            self.fun = fun
+            self.infinite = infinite
+            self.repeats = repeats
+            self.count = 0
+
+            self.finished = False
+
+        def call(self, emitter, event, *args):
+            self.fun(emitter, event, args)
+
+            self.count = self.count + 1
+            if not self.infinite and self.count > self.repeats:
+                self.finished = True
+            
 
 
 if __name__ == '__main__':
@@ -22,16 +72,15 @@ if __name__ == '__main__':
     def printAsFunction(emitter, event, args):
         print emitter, 'was told to', event, ''.join([' '.join(args), '.'])
         
-    ps.on('say', lambda emitter, event, args: printAsFunction(emitter, event, args or ["nothing"]))
+    ps.once('say', lambda emitter, event, args: printAsFunction(emitter, event, args or ["nothing"]))
 
     ps.emit('say', ['hello'])
+    ps.emit('say', ['hello'])
 
+    ps.on('say', lambda emitter, event, args: printAsFunction(emitter, event, args or ["nothing"]), False, 3)
     ps.emit('say') # no args
+    ps.emit('say', ['hello'])
     
     ps.emit('Something you are not prepared for')
     ps.emit('Something you are not prepared for', ['with arguments'])
 
-
-    # 12/14/2012: prints:
-    # <__main__.PubSub object at 0x09749D30> was told to say hello.
-    # <__main__.PubSub object at 0x09749D30> was told to say nothing.
